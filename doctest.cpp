b13405c39e7b823b16a28ee040193ab9405dbe1c
Todd Mostak <tmostak@gmail.com>
2013-04-04 09:49:47 -0400

    Changed doctest to accept multiple files-to-be-compressed as command line arguments

--- doctest.cpp
+++ doctest.cpp
@@ -3,35 +3,48 @@
 #include "Rice.h"
 
 using namespace std;
-int main() {
-    Docs docs("samplebins.dat");
-    unsigned int numDocs = docs.getNumDocs();
-    unsigned int unCompBits = 0;
-    unsigned int compBits = 0;
-    unsigned int numItems = 0;
-    for (int i = 0; i != numDocs; ++i) {
-        unsigned int *data;
-        size_t dataSize;
-        docs.getDoc(i, data, dataSize);
-        if (dataSize > 1) { // doc 41762 is one with all empty tweets
-            
-            Rice rice;
-            int status = rice.riceEncode(data, dataSize); // will find median by default
-            if (status == 0) {
-                unsigned int riceBits = rice.size();
-                unsigned int origBits =dataSize * 32;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        cout <<"Usage: " << argv[0] << " <file-to-be-compressed1> <file-to-be-compressed2>...\n";
+    }
+    else {
+        cout << endl; 
+        for (int d = 1; d != argc; ++d) {
+            cout << argv[d] << endl;
+            Docs docs(argv[d]);
+            unsigned int numDocs = docs.getNumDocs();
+            unsigned int unCompBits = 0;
+            unsigned int compBits = 0;
+            unsigned int numItems = 0;
+            for (int i = 0; i != numDocs; ++i) {
+                unsigned int *data;
+                size_t dataSize;
+                docs.getDoc(i, data, dataSize);
+                if (dataSize > 1) { // doc 41762 is one with all empty tweets
+                    
+                    Rice rice;
+                    int status = rice.riceEncode(data, dataSize); // will find median by default
+                    if (status == 0) {
+                        unsigned int riceBits = rice.size();
+                        unsigned int origBits =dataSize * 32;
 
-                float compRatio = origBits / float(riceBits);
-                if (compRatio > 1.0) {
-                    compBits += riceBits + 4;
-                    unCompBits += origBits;
-                    numItems += dataSize;
+                        float compRatio = origBits / float(riceBits);
+                        if (compRatio > 1.0) {
+                            compBits += riceBits + 4;
+                            unCompBits += origBits;
+                            numItems += dataSize;
+                        }
+                    }
                 }
             }
+            float finalRatio = unCompBits / float(compBits);
+            cout << argv[d] << ": " << endl;
+            cout << "Total items: " << numItems << endl;
+            cout << "Original size: " << numItems*4 << endl;
+            cout << "Final Size: " << compBits / 8 << endl;
+            cout << "Compression ratio: "<< finalRatio << endl;
+            cout << "Size compressed/original: " << 1.0 / finalRatio << endl; 
+            cout << "-------------------------------------------" << endl << endl;
         }
     }
-    float finalRatio = unCompBits / float(compBits);
-    cout << "Total items: " << numItems << endl;
-    cout << "Final Size: " << compBits << " " << finalRatio << endl;
-    cout << "Reverse ratio: " << 1.0 / finalRatio << endl; 
 }
